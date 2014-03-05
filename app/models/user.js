2178022a1f64313c9399f81f63552c0c3dd5ba93
Shaba Abhiram <shabarivas@gmail.com>
2014-03-04 18:08:28 -0800

    User model changes

--- app/models/user.js
+++ app/models/user.js
@@ -1,77 +1,56 @@
 var
     Mongoose        = require("mongoose"),
+    Bcrypt          = require("bcrypt"),
     Async           = require("async"),
-    Schema          = Mongoose.Schema,
-    Bcrypt          = require("bcrypt");
+    SALT_FACTOR     = 10;
 
-// TEMP CODE - REMOVE ME
-var config = {
-    SALT_FACTOR: 10
-};
-// END TEMP CODE
+var UserSchema = Mongoose.Schema({
+    username: {
+        type:       String,
+        required:   true,
+        unique:     true
+    },
+    email: {
+        type:       String,
+        required:   true,
+        unique:     true
+    },
+    crypted_pw: {
+        type:       String,
+        required:   true
+    }
+});
 
-/*****************************************************************************\
-User Model
+UserSchema.pre("save", function(callback) {
+    var _current_user = this;
 
-A user *should* have the following properties which is defined in the _schema
-variable below:
-    1. email:       unique id to identify user's by
-    2. password:    string (TODO: Enforce min pw requirements)
-\*****************************************************************************/
-var User = function() {
+    // Do nothing if the p/w is the same
+    if(_current_user.isModified("crypted_pw") === false) {
+        return callback();
+    }
 
-    // Define the schema for the User object
-    var _schema = new Schema({
-        email: { type: String, index: { unique: true, required: true } },
-        password: { type: String, required: true }
-    });
-    
-    // Get an instance of a "user" model using the above schema
-    var _model = Mongoose.model("user", _schema);
-
-    // Define how the user registers with an email / password
-    var _register = function(email, password, callback) {
-        _model.create({ email: email, password: password }, function(error, user) {
-            if(error) {
-                callback(error);
-            } else {
-                callback(null, user);
-            }
-        });
-    };
-
-    // We want to inject our hash when storing the p/w
-    _schema.pre("save", function(callback) {
-        var _current_user = this;
-
-        // Do nothing if the p/w is the same
-        if(_current_user.isModified("password") === false) {
-            return callback();
+    // Store the p/w after hashing it
+    Async.waterfall([
+        // Generate salt
+        function generate_salt(next) {
+            Bcrypt.genSalt(SALT_FACTOR, next);
+        },
+        function hash_pw(salt, next) {
+            Bcrypt.hash(_current_user.crypted_pw, salt, next);
+        },
+        function set_pw(hash, next) {
+            _current_user.crypted_pw = hash;
+            next();
         }
-
-        // Store the p/w after hashing it
-        Async.waterfall([
-            // Generate salt
-            function generate_salt(next) {
-                Bcrypt.genSalt(config.SALT_FACTOR, next);
-            },
-            function hash_pw(salt, next) {
-                Bcrypt.hash(_current_user.password, salt, next);
-            },
-            function set_pw(hash, next) {
-                _current_user.password = hash;
-                next();
-            }
-        ], function(error, result) {
-            callback(error);
-        });
+    ], function(error, result) {
+        callback(error);
     });
+});
 
-    return {
-        schema:         _schema,
-        register:       _register,
-        model:          _model,
-    };
+UserSchema.methods.compare_password = function(password_attempt, callback) {
+    Bcrypt.compare(password_attempt, this.crypted_pw, function(error, match) {
+        callback(error, match);
+    });
 };
 
-module.exports = User();
+module.exports = Mongoose.model("User", UserSchema);
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -10,15 +10,21 @@ var
 //
 // Test group to validate the user model
 //
-describe("[Models] - User registration", function() {
+describe("[Models] - User registration, finding users", function() {
     //
     // Setup
     //
     before(function(done) {
         Mongoose.connect("mongodb://172.12.8.155/user_test");
         // Lets seed the db with a test user
-        User.register("test@test.com", "9a$$w0Rd", function(error, user) {
-            done(error);
+        var test_user = new User({
+            username: "test_user",
+            email: "test_user@test.com",
+            crypted_pw: "password"
+        });
+        test_user.save(function(error) {
+            should.equal(error, null);
+            done();
         });
     });
 
@@ -26,7 +32,7 @@ describe("[Models] - User registration", function() {
     // Cleanup
     //
     after(function(done) {
-        User.model.remove({}, function() {
+        User.remove({}, function() {
             Mongoose.connection.close();
             done();
         });
@@ -42,24 +48,113 @@ describe("[Models] - User registration", function() {
         next_test();
     });
 
-    // Validate that a new user with a unique email can sign up
-    it("register new user", function(next_test) {
-        User.register("test2@test.com", "pa$Sw0rd", function(error, user) {
-            should.equal(null, error);
-            user.email.should.equal("test2@test.com");
-            user.password.should.not.equal("pa$Sw0rd");
+    // Validate that the test user is in the system
+    // find using the username
+    it("find test_user by username", function(next_test) {
+        User.findOne({username: "test_user"}, function(error, user) {
+            user.username.should.match("test_user");
+            user.email.should.match("test_user@test.com");
+            user.crypted_pw.should.not.match("password");
             next_test(error);
         });
     });
 
-    // Validate that a new user with an already used email cannot
-    // sign up
-    it("fail registration if duplicate email is used", function(next_test) {
-        User.register("test@test.com", "password", function(error, user) {
+    // Validate that test_user can be found using email
+    it("find test_user by email", function(next_test) {
+        User.findOne({email: "test_user@test.com"}, function(error, user) {
+            user.username.should.match("test_user");
+            user.email.should.match("test_user@test.com");
+            user.crypted_pw.should.not.match("password");
+            next_test(error);
+        });
+    });
+
+    // Validate that invalid_user is not in the system
+    it("find invalid user should fail", function(next_test) {
+        User.findOne({username: "invalid_user"}, function(error, user) {
+            should.equal(error, null);
+            should.equal(user, null);
+            next_test(error);
+        });
+    });
+
+    // Create a new user
+    it("Add a new user", function(next_test) {
+        var new_user = new User({
+            username: "philip",
+            email: "philip@test.com",
+            crypted_pw: "putter"
+        });
+        new_user.save(function(error) {
+            should.equal(error, null);
+            next_test(error);
+        });
+    });
+
+    // Create user without email should fail
+    it("Add a new user without a username", function(next_test) {
+        var new_user = new User({
+            email: "philip@test.com",
+            crypted_pw: "putter"
+        });
+        new_user.save(function(error) {
+            error.name.should.match("ValidationError");
+            error.errors.username.path.should.match("username");
+            next_test();
+        });
+    });
+
+    // Create user without email should fail
+    it("Add a new user without email", function(next_test) {
+        var new_user = new User({
+            username: "philip",
+            crypted_pw: "putter"
+        });
+        new_user.save(function(error) {
+            error.name.should.match("ValidationError");
+            error.errors.email.path.should.match("email");
+            next_test();
+        });
+    });
+
+    // Create user without password should fail
+    it("Add a new user without password", function(next_test) {
+        var new_user = new User({
+            username: "philip",
+            email: "philip@test.com"
+        });
+        new_user.save(function(error) {
+            error.name.should.match("ValidationError");
+            error.errors.crypted_pw.path.should.match("crypted_pw");
+            next_test();
+        });
+    });
+
+    // Ensure philip exists in db
+    it("Verify new user", function(next_test) {
+        User.findOne({
+            username: "philip",
+            email: "philip@test.com"
+        }, function(error, user) {
+            should.equal(error, null);
+            user.username.should.match("philip");
+            user.email.should.match("philip@test.com");
+            user.crypted_pw.should.not.match("putter");
+            next_test(error);
+        });
+    });
+
+    // Re-adding an existing user should fail
+    it("Re-save existing user should fail", function(next_test) {
+        var existing_user = new User({
+            username: "philip",
+            email: "philip2@test.com",
+            crypted_pw: "driver"
+        });
+        existing_user.save(function(error) {
             error.code.should.eql(11000);
             error.name.should.match("MongoError");
             error.err.should.startWith("E11000 duplicate key error");
-            should.equal(null, user);
             next_test();
         });
     });

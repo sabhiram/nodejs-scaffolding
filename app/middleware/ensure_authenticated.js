31ab9f954bbb7921d24dc447b8efc2dab7d23a15
Shaba Abhiram <shabarivas@gmail.com>
2014-03-05 23:12:54 -0800

    Authentication validation for persistent user connection, middleware validates if the user is auth'd, if not routes to the login page

---
+++ app/middleware/ensure_authenticated.js
@@ -0,0 +1,10 @@
+/*****************************************************************************\
+Simple ensure authenticated middleware, which is responsible for validating
+that a user is authenticated, else forwards to the login page.
+\*****************************************************************************/
+module.exports = function(request, response, next_route) {
+    if(request.isAuthenticated()) {
+        return next_route();
+    }
+    response.redirect('/login');
+};

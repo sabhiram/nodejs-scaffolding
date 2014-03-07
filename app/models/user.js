0a2ea0051e47cd4f915ff581cf8fa70258836dda
Shaba Abhiram <shabarivas@gmail.com>
2014-03-06 20:24:44 -0800

    Updating user model formatting + more comments

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,21 @@
 var
-    Mongoose        = require("mongoose"),
-    Bcrypt          = require("bcrypt"),
+    // mongoose - 
+    //     A node module to interface w/ mongoDB
+    mongoose        = require("mongoose"),
+    
+    // bcrypt -
+    //     Someone say crypto?
+    bcrypt          = require("bcrypt"),
+    
+    // Other utils
     Async           = require("async"),
+
+    // Globals
     SALT_FACTOR     = 10;
 
-var UserSchema = Mongoose.Schema({
+
+// Define the user schema
+var UserSchema = mongoose.Schema({
     username: {
         type:       String,
         required:   true,
@@ -15,42 +26,46 @@ var UserSchema = Mongoose.Schema({
         required:   true,
         unique:     true
     },
-    crypted_pw: {
+    password: {
         type:       String,
         required:   true
     }
 });
 
+// Attach a custom handler on the save event which hashes
+// the users password if it has changed
 UserSchema.pre("save", function(callback) {
     var _current_user = this;
 
     // Do nothing if the p/w is the same
-    if(_current_user.isModified("crypted_pw") === false) {
+    if(_current_user.isModified("password") === false) {
         return callback();
     }
-
+    
     // Store the p/w after hashing it
     Async.waterfall([
         // Generate salt
         function generate_salt(next) {
-            Bcrypt.genSalt(SALT_FACTOR, next);
+            bcrypt.genSalt(SALT_FACTOR, next);
         },
         function hash_pw(salt, next) {
-            Bcrypt.hash(_current_user.crypted_pw, salt, next);
+            bcrypt.hash(_current_user.password, salt, next);
         },
         function set_pw(hash, next) {
-            _current_user.crypted_pw = hash;
+            _current_user.password = hash;
             next();
-        }
+        },
     ], function(error, result) {
         callback(error);
     });
 });
 
+// Define a compare_password method which allows us to check a
+// password attempt against the user database with ease.
 UserSchema.methods.compare_password = function(password_attempt, callback) {
-    Bcrypt.compare(password_attempt, this.crypted_pw, function(error, match) {
+    bcrypt.compare(password_attempt, this.password, function(error, match) {
         callback(error, match);
     });
 };
 
-module.exports = Mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
